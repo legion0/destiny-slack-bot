29aaba128ed353e3a56d52607a4061df9b3b7f2c
Jonathan K <phenixdoc@gmail.com>
2020-03-24 16:25:05 -0700

    working responder

---
+++ Code.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const SLACK_ACCESS_TOKEN = PropertiesService.getScriptProperties().getProperty('SLACK_ACCESS_TOKEN');
+const LOGS_SPREADSHEET_ID = PropertiesService.getScriptProperties().getProperty('LOGS_SPREADSHEET_ID');
+const LOG_TO_SHEET = true;
+const LOG_PAYLOAD = true;
+
+var POST_MESSAGE_ENDPOINT = 'https://slack.com/api/chat.postMessage';
+var TEXTS = [
+	'Do you like dogs?:dog:',
+	'You said the word!:grinning:',
+	'I hope you like this...:poodle:',
+	'Dogs are cute!:dog2: Dogs are cute!:dog2:',
+	'I know, you want this.:thumbsup:'
+];
+
+/*
+* Use this function to replay post to the dev version of the script
+*/
+function testPost() {
+	let payload = { "event": { "text": "test message" } };
+
+	doPost({
+		'postData': {
+			'contents': JSON.stringify(payload)
+		}
+	});
+}
+
+function handlePayload(payload) {
+	if (payload.event.bot_id) {
+		// we ignore bots for now, maybe limit to messages from self later on.
+		return;
+	}
+
+	if (payload.event.text.match(/dog/)) {
+		postToSlack(payload.event);
+	}
+}
+
+function doPost(event) {
+	try {
+		if (LOG_PAYLOAD) {
+			console_log('event', event);
+		}
+
+		let payload = JSON.parse(event.postData.contents);
+		if (payload.challenge) {
+			return ContentService.createTextOutput(payload.challenge);
+		}
+
+		if (LOG_PAYLOAD) {
+			console_log('payload', payload); // TODO: remove
+		}
+
+		console_log('Object.keys(payload)', Object.keys(payload));
+		if (payload.event) {
+			console_log('Object.keys(payload.event)', Object.keys(payload.event));
+		}
+
+		handlePayload(payload);
+
+	} catch (err) {
+		console_log(err.name + ': ' + err.message);
+	}
+}
+
+
+function postToSlack(event) {
+	let text = TEXTS[Math.floor(Math.random() * TEXTS.length)];
+	let payload = {
+		'token': SLACK_ACCESS_TOKEN,
+		'channel': event.channel,
+		'text': text
+	};
+	UrlFetchApp.fetch(POST_MESSAGE_ENDPOINT, { 'method': 'post', 'payload': payload });
+}
+
+
+let _consoleLogOrig = console_log;
+
+
+function console_log() {
+	if (LOG_TO_SHEET) {
+		logToSheet(...arguments);
+	}
+	console.log(...arguments);
+}
+
+function logToSheet() {
+	let args = Array.prototype.slice.call(arguments);
+	let logMsg = "";
+	if (args.length == 1 && typeof (args[0]) == 'string') {
+		logMsg = args[0];
+	} else {
+		logMsg = JSON.stringify(args, undefined, 2);
+	}
+
+
+	var sheet = SpreadsheetApp.openById(LOGS_SPREADSHEET_ID).getSheetByName('logs');
+	sheet.appendRow([new Date(), logMsg]);
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Title
+
+## Links
+
+* https://api.slack.com/bot-users#installing-bot
+* https://api.slack.com/tools
+* https://medium.com/@yusuke8h/easy-slack-bot-from-google-apps-script-82065b5e08f7
+
+* https://www.freeformatter.com/json-escape.html
+* http://json.parser.online.fr/
+* https://reqbin.com/
